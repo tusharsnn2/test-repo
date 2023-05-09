0fd740100ff01c68e0f79c0642ed3a10dff57116
Tushar Singh <tusharvickey1999@gmail.com>
2023-05-09 11:29:09 +0530

    initial commit

---
+++ script.sh
@@ -0,0 +1,5 @@
+for base_pr_id in $(gh pr list --search "head:$BASE_PR_REF" --json number --jq '.[].number'); do 
+    if [ $MAIN_BRANCH_NAME == "$( gh pr view $base_pr_id --json baseRefName  --jq '.baseRefName')" ]; then
+        gh pr ready $base_pr_id --undo 
+    fi
+done
